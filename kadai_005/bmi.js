244a9d46e54e115b0c359395be3d38abc791841b
Sasaki-9969 <minihito12@gmail.com>
2025-05-21 23:30:48 +0900

    kadai_005を再プッシュ

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,6 @@
-let weight = '68';
-let height = '1.7';
-let bmi = weight / (height * height)
+let weight = 68;
+let height = 1.7;
+let bmi = weight / (height * height);
 
 console.log(weight);
 console.log(height);
