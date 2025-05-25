ef57a1d6dea438258ae23a28f04dc130c95d9af6
Sasaki-9969 <minihito12@gmail.com>
2025-05-26 00:34:07 +0900

    kadai_mdn_jsを再プッシュ

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -4,6 +4,4 @@ const year = today.getFullYear ();
 const month = today.getMonth () +1;
 const date = today.getDate ();
 
-const formatted = `${year}年${month}月${date}日`;
-
-console.log(formatted);
+console.log(year + "年" + month + "月" + date + "日");
